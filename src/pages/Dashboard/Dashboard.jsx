345bd0bbda4e83261f8c173ddf5841cc7a8eea92
suada koka <kokasuada8@gmail.com>
2024-06-24 17:58:18 +0200

    inital message

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export function Dashboard() {
+  return (
+    <div className="text">
+      <div className="navbar">
+        <ul>
+          <li>Name</li>
+          <li>Email</li>
+          <li>Phone</li>
+          <li>Enroll Number</li>
+          <li>Date of admission</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default Dashboard;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+
+export function Login() {
+  const navigate = useNavigate();
+  function handleclick() {
+    navigate("/dashboard");
+  }
+
+  return (
+    <div className="title">
+      <form action="">
+        <div className="sign">
+          <p>
+            SIGN IN <a>Enter your credintials to access your account</a>
+          </p>
+        </div>
+
+        <div className="input-box">
+          <a>Email</a>
+          <input type="text" placeholder="Enter your email" required />
+        </div>
+
+        <div className="input-box">
+          <a>Password</a>
+          <input type="password" placeholder="Enter your password" required />
+        </div>
+        <button onClick={handleclick} type="submit">
+          SIGN IN
+        </button>
+        {/* <div className="forgot">
+          <label>
+            <input type="checkbox" />
+            Forgot you password?
+          </label>
+          <a href="#">Reset Password</a>
+        </div>  */}
+      </form>
+    </div>
+  );
+}
+
+export default Login;
